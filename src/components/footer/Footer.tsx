8377cf14061c4fb96d1c29b82acca19bfab618fd
Rodrigo Henrique <rhs.buiu@gmail.com>
2025-05-22 14:19:03 -0300

    conclusão da atividade 22/05, estilização das páginas de cadastro e login

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,21 +1,27 @@
-import { FacebookLogoIcon, GithubLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from '@phosphor-icons/react'
+import { GithubLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from '@phosphor-icons/react'
 
 function Footer() {
 
-    let data = new Date().getFullYear()
+    const data = new Date().getFullYear()
 
     return (
         <>
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
-                            Blog Pessoal Generation | Copyright: {data}
-                        </p>
+                        Blog Pessoal Generation | Copyright: {data}
+                    </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <LinkedinLogoIcon size={48} weight='bold' />
-                        <InstagramLogoIcon size={48} weight='bold' />
-                        <GithubLogoIcon size={48} weight='bold' />
+                        <a href="https://www.linkedin.com/in/rodrigohenrikeh/" target="_blank">
+                            <LinkedinLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.instagram.com/rodrigohenrikeh/" target="_blank">
+                            <InstagramLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://github.com/RodrigoHenrikeH" target="_blank">
+                            <GithubLogoIcon size={48} weight='bold' />
+                        </a>
                     </div>
                 </div>
             </div>
---
+++ src/services/Service.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-wrapper-object-types */
+/* eslint-disable @typescript-eslint/no-unsafe-function-type */
+
+
+
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "https://blogpessoal-5edl.onrender.com"
+})
+
+
+export const cadastrarUsuario = async ( url: string, dados: Object, setDados: Function ) => {
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data)
+}
+
+export const login = async (url: string, dados: Object, setDados: Function) => {
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data)
+}
